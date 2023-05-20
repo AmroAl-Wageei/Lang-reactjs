f8402991c7ef4c3e87aa7aa2183aef0846bc575c
Amro Al-Wageei <shandaqamro@gmail.com>
2023-05-21 00:25:02 +0300

    MultiLanguages

--- lang/src/App.js
+++ lang/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const inlineStyles = {
+    fontSize: "16px",
+    fontWeight: "bold",
+    display: "flex",
+    aliginItem: "center",
+    justifyContent: "center",
+    gap: "20px",
+    marginTop: "30px",
+  };
+
+  const inl = {
+    fontSize: "40px",
+    display: "flex",
+    aliginItem: "center",
+    justifyContent: "center",
+    marginTop: "30px",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header style={inlineStyles}>
+        {i18n.language == "en" && (
+          <button
+            className="ar"
+            onClick={() => {
+              i18n.changeLanguage("ar");
+            }}
+          >
+            Arabic
+          </button>
+        )}
+
+        {i18n.language == "ar" && (
+          <button
+            className="en"
+            onClick={() => {
+              i18n.changeLanguage("en");
+            }}
+          >
+            English
+          </button>
+        )}
       </header>
+      <div className="yes">
+        <h1 style={inl}>{t("title")}</h1>
+      </div>
+
+      <section>
+        <div className="container">
+          <div className="left">{t("salimeen")} </div>
+          <div className="right">{t("thank")} </div>
+        </div>
+      </section>
     </div>
   );
 }
